28d5d34d5ac70bc5746fb6f2cd4c21b2ee45c076
vasili <frugas@list.ru>
2023-05-16 09:48:50 +0300

    need_fix

--- entities/Post.js
+++ entities/Post.js
@@ -1,17 +1,51 @@
-export default class Post {
-    constructor (title, body) {
-      this.title = title,
-      this.body = body,
-      this.id = Post.generateId()
-    }
-  
-    static generateId () {
-      if (!Post.currectId) {
-        Post.currectId = 1
-      } else {
-        Post.currectId ++
-      }
-  
-      return Post.currectId
-    }
+import { Sequelize } from 'sequelize';
+import { DataTypes } from 'sequelize';
+// Создание объекта sequelize для установки соединения
+const sequelize = new Sequelize('MyBlogAPI', 'myblogdb', '100797', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+// Проверка соединения
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Соединение с базой данных успешно установлено');
+  })
+  .catch((error) => {
+    console.error('Ошибка подключения к базе данных:', error);
+  });
+
+
+// Определение модели User
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'username' // Указываем соответствующее поле в таблице
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'password' // Указываем соответствующее поле в таблице
   }
+}, {
+  tableName: 'D_B' // Указываем имя существующей таблицы
+});
+
+// Получение всех пользователей
+User.findAll()
+  .then(users => {
+    console.log('Все пользователи:', users);
+  })
+  .catch(error => {
+    console.error('Ошибка при получении пользователей:', error);
+  });
+
+// Экспорт модели
+module.exports = { User, sequelize };
